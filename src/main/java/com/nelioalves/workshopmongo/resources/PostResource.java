efeef8e7e770d7d8f562a46fa1cf9f9f6ad2b523
Gabriel Carvalho <gabriiell0710@gmail.com>
2023-01-20 12:54:48 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/nelioalves/workshopmongo/resources/PostResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/PostResource.java
@@ -19,7 +19,7 @@ public class PostResource {
 	
 	@RequestMapping(value="/{id}",method=RequestMethod.GET)
 	public ResponseEntity<Post> findById(@PathVariable String id){
-		Post obj = service.findById(id);
+		Post obj = service.findById(id); 
 		
 		return ResponseEntity.ok().body(obj);
 	}
